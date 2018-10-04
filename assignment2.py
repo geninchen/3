ae6c8199603853d8b3086c7c3c4473d4704ee7eb
geninchen <43598957+geninchen@users.noreply.github.com>
2018-10-04 15:19:31 +0800

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,9 @@
+＃assignment2
+
+# set d=10x = "There are %d types of people." % 10
+#set binary as "binary"binary = "binary"
+#set do_not as "don't"do_not = "don't"
+#set first"s"= binary and the second "s"= do_noty = "Those who know %s and those who %s." % (binary, do_not)
+#print xprint(x)#print yprint(y)#print sentance with variable x which is "There are 10 types of people."print("I said: %r") % x#print sentance with variable y which is "Those who know binary and those who don't."print("I also said: '%s'") % y
+#set variable hilarious as Falsehilarious = False#set "joke_evaluation" with undefined variable rjoke_evaluation = "Isn't that joke so funny?! %r" 
+#print the sentanceprint(joke_evaluation % hilarious)#set w = This is the left side of...w = "This is the left side of..."#set e = a string with a right side.e = "a string with a right side."#combine sentances that w and e represented and print itprint(w + e)#2.Find all the places where a string is put inside a string. There are four places.##Line 10,18,20,28,36#3. Are you sure there are only four places? How do you know? Maybe I like lying.##No,There are 5 places.#4.Explain why adding the two strings w and e with + makes a longer string.## Since the sentances represented by w and e are combining together.
